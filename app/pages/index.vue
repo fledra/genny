fe39d3b7b57bc5e645c2675e4aff52c2328c20ef
Fledra <baran@fledra.dev>
2025-10-27 20:26:37 +0300

    added hero to homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    hello world!
-  </div>
+  <UPageHero
+    title="Mock smarter, not harder"
+    description="
+      A simple, intuitive GUI for Faker.js — generate realistic fake data instantly.
+      Perfect for developers, testers, and designers who need quick mock data without writing code.
+    "
+  >
+    <template #footer>
+      <p class="pt-4">
+        Get started by picking a module from the left.
+      </p>
+    </template>
+  </UPageHero>
 </template>
