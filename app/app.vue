c665c4288342f982b19b8264b2519a64575f0f04
Fledra <baran@fledra.dev>
2025-11-03 18:57:01 +0300

    general style updates

--- app/app.vue
+++ app/app.vue
@@ -7,7 +7,7 @@
     <UMain class="pt-4">
       <UContainer class="flex gap-4">
         <Navigation class="max-w-3xs" />
-        <NuxtPage class="grow" />
+        <NuxtPage class="grow pl-4 pt-4" />
       </UContainer>
     </UMain>
   </UApp>
