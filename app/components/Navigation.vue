44f4117e69f91d795d3431cc99fc05e3cb0982df
Fledra <baran@fledra.dev>
2025-11-03 19:12:05 +0300

    updated navigation to use route data

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -8,16 +8,32 @@
 import type { NavigationMenuItem } from '@nuxt/ui';
 
 const router = useRouter();
-const items = computed<NavigationMenuItem[]>(() => {
-  const routes = router.getRoutes();
-  return routes.map<NavigationMenuItem>((route) => {
-    const label = route.name === 'index' ? 'Home' : (route.name?.toString() ?? '');
-    const icon = route.meta.icon ?? '';
-    return {
-      label,
-      icon,
+const routes = router.getRoutes();
+
+function getRoutesOf(path: string): NavigationMenuItem[] {
+  return routes
+    .filter((route) => route.path.includes(path))
+    .map((route) => ({
+      label: route.name?.toString(),
+      icon: route.meta.icon ?? undefined,
       to: route.path,
-    };
-  });
-});
+    }));
+}
+
+const items: NavigationMenuItem[] = [
+  {
+    label: 'Home',
+    to: '/',
+  },
+  {
+    label: 'Modules',
+    type: 'label',
+  },
+  ...getRoutesOf('modules/'),
+  {
+    label: 'Primitives',
+    type: 'label',
+  },
+  ...getRoutesOf('primitives/'),
+];
 </script>
